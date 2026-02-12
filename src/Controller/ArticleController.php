667a8f93578cdd5ee623112b2ff493ee039bca55
Maxence <maxence.coste.mc@gmail.com>
2026-02-12 10:05:20 +0100

    TD3 Ex 5.1 : Suppression d'un article (DELETE)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ArticleController extends AbstractController
@@ -15,5 +18,14 @@ class ArticleController extends AbstractController
         $articles = $articleRepository->findAll();
 
         return $this->json($articles, 200, [], ['groups' => ['article:read']]);
+    }
+
+    #[Route('/api/articles/{id}', name: 'app_api_article_delete', methods: ['DELETE'])]
+    public function delete(Article $article, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
     }
 }
