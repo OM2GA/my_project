6deb7085e81faacf7b62fb3cb8d07b80bc16cb84
Maxence <maxence.coste.mc@gmail.com>
2026-02-12 11:06:30 +0100

    TD5 Etape 2 : RegistrationController fonctionnel avec User+Person

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -20,19 +20,32 @@ class RegistrationController extends AbstractController
     {
         $data = json_decode($request->getContent(), true);
 
+        // Vérification de sécurité
+        if (empty($data['email']) || empty($data['password'])) {
+            return new JsonResponse(['error' => 'Email and password are required'], Response::HTTP_BAD_REQUEST);
+        }
+
+        // 1. Création de l'Adresse
         $address = new Address();
         $address->setStreet($data['address']['street'] ?? 'Rue inconnue');
         $address->setZipcode($data['address']['zipcode'] ?? '00000');
         $address->setCity($data['address']['city'] ?? 'Ville inconnue');
 
+        // 2. Création de la Personne
         $person = new Person();
         $person->setFirstname($data['firstname'] ?? 'John');
         $person->setLastname($data['lastname'] ?? 'Doe');
-        $person->setBirthdate(new \DateTime($data['birthdate'] ?? '2000-01-01'));
-        $person->setAddress($address); 
+        // On gère la date proprement
+        try {
+            $person->setBirthdate(new \DateTime($data['birthdate'] ?? 'now'));
+        } catch (\Exception $e) {
+            $person->setBirthdate(new \DateTime('now'));
+        }
+        $person->setAddress($address);
 
+        // 3. Création du User
         $user = new User();
-        $user->setEmail($data['email']);
+        $user->setEmail($data['email']); // <--- C'est ici que c'est important !
         $user->setPassword(
             $userPasswordHasher->hashPassword(
                 $user,
@@ -40,9 +53,12 @@ class RegistrationController extends AbstractController
             )
         );
         
+        // 4. Liaison User -> Person
         $user->setPerson($person);
 
-        $entityManager->persist($person); 
+        // 5. Sauvegarde
+        $entityManager->persist($address);
+        $entityManager->persist($person);
         $entityManager->persist($user);
         $entityManager->flush();
 
