b7263a62409e11e156f79490f12aaefe8eee948c
Maxence <maxence.coste.mc@gmail.com>
2026-02-12 10:08:21 +0100

    TD3 Ex 5.2 : Modification d'un article (PUT)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,6 +7,7 @@ use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -27,5 +28,22 @@ class ArticleController extends AbstractController
         $entityManager->flush();
 
         return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/articles/{id}', name: 'app_api_article_update', methods: ['PUT'])]
+    public function update(Article $article, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['title'])) {
+            $article->setTitle($data['title']);
+        }
+        if (isset($data['content'])) {
+            $article->setContent($data['content']);
+        }
+
+        $entityManager->flush();
+
+        return $this->json($article, 200, [], ['groups' => ['article:read']]);
     }
 }
